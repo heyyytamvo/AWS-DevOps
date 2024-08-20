fae2a45fbe9acae4cb66cc98cf67c8b9df2c23b9
Tam Vo <tamvoforwork@gmail.com>
2024-08-20 21:20:49 +0700

    Update Microservice App

---
+++ Microservices/nestjs-microservices-docker/order-microservice/src/config.ts
@@ -0,0 +1,6 @@
+export const db_host = process.env.DB_HOST || 'microservice.mv4kp.mongodb.net';
+export const db_name = process.env.DB_NAME || 'ordermgmt';
+export const order_host = process.env.ORDER_HOST || 'order';
+export const payment_host = process.env.PAYMENT_HOST || 'payment';
+export const db_user = process.env.DB_USER || 'microserviceuser';
+export const db_password = process.env.db_password || 'thisispassword';
